f7134da0d17c853e7c9190f85929b91873b14764
manq2010 <manq2010@gmail.com>
2022-11-02 18:37:58 +0200

    Add syling

--- src/index.js
+++ src/index.js
@@ -1,6 +1,13 @@
 import _ from 'lodash'; // eslint-disable-line
 import './style.css';
 
+class Tasks {
+  constructor(description, completed) {
+    this.description = description;
+    this.completed = completed;
+  }
+}
+
 // const toToDoArray = [
 //   {
 //     index: 0,
@@ -30,29 +37,26 @@ import './style.css';
 // ];
 
 
-const AddTask = (task) => {
+const addTask = (task) => {
   // const liMarkup = toToDoArray.map((task) => `
 
   const clearCompleted = document.querySelector('.clear-completed');
   const liMarkup = `
     <li class="task-item">
-        <div class="btn-check">
-            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-app"
-                viewBox="0 0 16 16">
-                <path
-                    d="M11 2a3 3 0 0 1 3 3v6a3 3 0 0 1-3 3H5a3 3 0 0 1-3-3V5a3 3 0 0 1 3-3h6zM5 1a4 4 0 0 0-4 4v6a4 4 0 0 0 4 4h6a4 4 0 0 0 4-4V5a4 4 0 0 0-4-4H5z" />
-            </svg>
-        </div>
-        <textarea name="" id="edit-text" spellcheck="false">${task}</textarea>
-        <div class="task-dots">
-        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
-            class="bi bi-three-dots-vertical" viewBox="0 0 16 16">
-            <path
-                d="M9.5 13a1.5 1.5 0 1 1-3 0 1.5 1.5 0 0 1 3 0zm0-5a1.5 1.5 0 1 1-3 0 1.5 1.5 0 0 1 3 0zm0-5a1.5 1.5 0 1 1-3 0 1.5 1.5 0 0 1 3 0z" />
-        </svg>
-        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-trash3" viewBox="0 0 16 16">
-            <path d="M6.5 1h3a.5.5 0 0 1 .5.5v1H6v-1a.5.5 0 0 1 .5-.5ZM11 2.5v-1A1.5 1.5 0 0 0 9.5 0h-3A1.5 1.5 0 0 0 5 1.5v1H2.506a.58.58 0 0 0-.01 0H1.5a.5.5 0 0 0 0 1h.538l.853 10.66A2 2 0 0 0 4.885 16h6.23a2 2 0 0 0 1.994-1.84l.853-10.66h.538a.5.5 0 0 0 0-1h-.995a.59.59 0 0 0-.01 0H11Zm1.958 1-.846 10.58a1 1 0 0 1-.997.92h-6.23a1 1 0 0 1-.997-.92L3.042 3.5h9.916Zm-7.487 1a.5.5 0 0 1 .528.47l.5 8.5a.5.5 0 0 1-.998.06L5 5.03a.5.5 0 0 1 .47-.53Zm5.058 0a.5.5 0 0 1 .47.53l-.5 8.5a.5.5 0 1 1-.998-.06l.5-8.5a.5.5 0 0 1 .528-.47ZM8 4.5a.5.5 0 0 1 .5.5v8.5a.5.5 0 0 1-1 0V5a.5.5 0 0 1 .5-.5Z"/>
-        </svg>
+      <div class="btn-check">
+          <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-app"
+              viewBox="0 0 16 16">
+              <path
+                  d="M11 2a3 3 0 0 1 3 3v6a3 3 0 0 1-3 3H5a3 3 0 0 1-3-3V5a3 3 0 0 1 3-3h6zM5 1a4 4 0 0 0-4 4v6a4 4 0 0 0 4 4h6a4 4 0 0 0 4-4V5a4 4 0 0 0-4-4H5z" />
+          </svg>
+      </div>
+      <textarea name="" id="edit-text" spellcheck="false">${task.description}</textarea>
+      <div class="task-dots">
+          <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
+              class="bi bi-three-dots-vertical delete-task" viewBox="0 0 16 16">
+              <path
+                  d="M9.5 13a1.5 1.5 0 1 1-3 0 1.5 1.5 0 0 1 3 0zm0-5a1.5 1.5 0 1 1-3 0 1.5 1.5 0 0 1 3 0zm0-5a1.5 1.5 0 1 1-3 0 1.5 1.5 0 0 1 3 0z" />
+          </svg>
       </div>
     </li>
     `;
@@ -69,7 +73,7 @@ const deleteTask = (element) => {
   element.remove();
 };
 
-const fucusTask = () => {
+const editTask = () => {
   // const taskItem = document.querySelector('.task-item');
   // const textArea = document.querySelector('textarea');
 
@@ -87,27 +91,87 @@ const fucusTask = () => {
   // });
 };
 
+// Local Storage Functions
+const getTasks = () => {
+  let tasks;
+
+  // let tasksT = JSON.parse(localStorage.getItem('tasks'));
+  // const taskIndex = taskT.children[1].innerText;
+
+  // console.log(taskIndex)
+
+  if (localStorage.getItem('tasks') === null) tasks = [];
+  else {
+    tasks = JSON.parse(localStorage.getItem('tasks'));
+  }
+
+  return tasks;
+};
+
+const addTaskLocalStorage = (task) => {
+  const tasks = getTasks();
+  tasks.push(task);
+  localStorage.setItem('tasks', JSON.stringify(tasks));
+};
+
+const deleteTaskLocalStorage = (element) => {
+  const tasks = getTasks();
+  tasks.forEach((task, index) => {
+    // console.log(index);
+    // console.log(element);
+    if (element === index) tasks.splice(index, 1);
+  });
+  localStorage.setItem('tasks', JSON.stringify(tasks));
+};
+
+const updateIndex = () => {
+  let tasks = JSON.parse(localStorage.getItem('tasks'));
+
+  //Modify elements
+  for (let i = 0; i < tasks.length; i++) {
+   console.log(tasks[i].description);
+  }
+
+  //Store modified array in localstorage
+  // localStorage.setItem('tasks', JSON.stringify(tasks));
+}
+
 window.document.addEventListener('DOMContentLoaded', (e) => {
-  // contentMarkup();
+
+  // e.preventDefault();
+
+  
+
+  const tasks = getTasks();
+  tasks.forEach((task) => addTask(task));
+
+  updateIndex();
+
   const addItem = document.querySelector('.add-item');
   const inputItem = document.querySelector('#input-item');
-  addItem.addEventListener('click', () => {
+
+  addItem.addEventListener('click', (e) => {
+    console.log(e)
+    const tasksObject = new Tasks(inputItem.value, false);
     // console.log(inputItem.value === '');
     if (inputItem.value !== '') {
-      AddTask(inputItem.value);
+      addTask(tasksObject);
+      addTaskLocalStorage(tasksObject);
+      clearTaskInputs();
+      // addTaskLocalStorage(inputItem.value);
     } else {
       //
     }
-    clearTaskInputs();
   });
 
   const taskDots = Array.from(document.getElementsByClassName('delete-task'));
 
-  taskDots.forEach((element) => {
+  taskDots.forEach((element, index) => {
     element.addEventListener('click', (e) => {
       // console.log(e.target.parentNode.parentNode)
       const node = e.target.parentNode.parentNode;
       deleteTask(node);
+      deleteTaskLocalStorage(index);
     });
   });
 });
