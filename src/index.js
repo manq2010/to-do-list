aa7f4273cecda05cd9751178417d90385011cb04
manq2010 <manq2010@gmail.com>
2022-11-02 22:31:36 +0200

    Add edit task function

--- src/index.js
+++ src/index.js
@@ -2,44 +2,14 @@ import _ from 'lodash'; // eslint-disable-line
 import './style.css';
 
 class Tasks {
-  constructor(description, completed) {
+  constructor(index, description, completed) {
+    this.index = index;
     this.description = description;
     this.completed = completed;
   }
 }
 
-// const toToDoArray = [
-//   {
-//     index: 0,
-//     description: 'cook',
-//     completed: true,
-//   },
-//   {
-//     index: 1,
-//     description: 'study',
-//     completed: true,
-//   },
-//   {
-//     index: 2,
-//     description: 'wash clothes',
-//     completed: true,
-//   },
-//   {
-//     index: 3,
-//     description: 'sleep',
-//     completed: true,
-//   },
-//   {
-//     index: 4,
-//     description: 'exercise',
-//     completed: true,
-//   },
-// ];
-
-
 const addTask = (task) => {
-  // const liMarkup = toToDoArray.map((task) => `
-
   const clearCompleted = document.querySelector('.clear-completed');
   const liMarkup = `
     <li class="task-item">
@@ -52,11 +22,11 @@ const addTask = (task) => {
       </div>
       <textarea name="" id="edit-text" spellcheck="false">${task.description}</textarea>
       <div class="task-dots">
-          <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
-              class="bi bi-three-dots-vertical delete-task" viewBox="0 0 16 16">
-              <path
-                  d="M9.5 13a1.5 1.5 0 1 1-3 0 1.5 1.5 0 0 1 3 0zm0-5a1.5 1.5 0 1 1-3 0 1.5 1.5 0 0 1 3 0zm0-5a1.5 1.5 0 1 1-3 0 1.5 1.5 0 0 1 3 0z" />
-          </svg>
+          <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-trash3 delete-task"
+                    viewBox="0 0 16 16">
+                    <path
+                        d="M6.5 1h3a.5.5 0 0 1 .5.5v1H6v-1a.5.5 0 0 1 .5-.5ZM11 2.5v-1A1.5 1.5 0 0 0 9.5 0h-3A1.5 1.5 0 0 0 5 1.5v1H2.506a.58.58 0 0 0-.01 0H1.5a.5.5 0 0 0 0 1h.538l.853 10.66A2 2 0 0 0 4.885 16h6.23a2 2 0 0 0 1.994-1.84l.853-10.66h.538a.5.5 0 0 0 0-1h-.995a.59.59 0 0 0-.01 0H11Zm1.958 1-.846 10.58a1 1 0 0 1-.997.92h-6.23a1 1 0 0 1-.997-.92L3.042 3.5h9.916Zm-7.487 1a.5.5 0 0 1 .528.47l.5 8.5a.5.5 0 0 1-.998.06L5 5.03a.5.5 0 0 1 .47-.53Zm5.058 0a.5.5 0 0 1 .47.53l-.5 8.5a.5.5 0 1 1-.998-.06l.5-8.5a.5.5 0 0 1 .528-.47ZM8 4.5a.5.5 0 0 1 .5.5v8.5a.5.5 0 0 1-1 0V5a.5.5 0 0 1 .5-.5Z" />
+          </svg> 
       </div>
     </li>
     `;
@@ -73,37 +43,17 @@ const deleteTask = (element) => {
   element.remove();
 };
 
-const editTask = () => {
-  // const taskItem = document.querySelector('.task-item');
-  // const textArea = document.querySelector('textarea');
-
-  // textArea.addEventListener('focus', (e) => {
-  //   console.log('focus');
-  //   taskItem.style.backgroundColor = "pink";
-  //   textArea.style.backgroundColor = "pink";
-
-  // });
-
-  // textArea.addEventListener('blur', (e) => {
-  //   console.log('blur')
-  //   taskItem.style.backgroundColor = "";
-  //   textArea.style.backgroundColor = "";
-  // });
+const editTask = (index, value) => {
+  const tasks = JSON.parse(localStorage.getItem('tasks'));
+  tasks[index].description = value;
+  localStorage.setItem('tasks', JSON.stringify(tasks));
 };
 
 // Local Storage Functions
 const getTasks = () => {
   let tasks;
-
-  // let tasksT = JSON.parse(localStorage.getItem('tasks'));
-  // const taskIndex = taskT.children[1].innerText;
-
-  // console.log(taskIndex)
-
   if (localStorage.getItem('tasks') === null) tasks = [];
-  else {
-    tasks = JSON.parse(localStorage.getItem('tasks'));
-  }
+  else tasks = JSON.parse(localStorage.getItem('tasks'));
 
   return tasks;
 };
@@ -117,48 +67,41 @@ const addTaskLocalStorage = (task) => {
 const deleteTaskLocalStorage = (element) => {
   const tasks = getTasks();
   tasks.forEach((task, index) => {
-    // console.log(index);
-    // console.log(element);
     if (element === index) tasks.splice(index, 1);
   });
   localStorage.setItem('tasks', JSON.stringify(tasks));
 };
 
 const updateIndex = () => {
-  let tasks = JSON.parse(localStorage.getItem('tasks'));
+  window.location.reload();
+  const tasks = JSON.parse(localStorage.getItem('tasks'));
 
-  //Modify elements
-  for (let i = 0; i < tasks.length; i++) {
-   console.log(tasks[i].description);
+  for (let i = 0; i < tasks.length; i += 1) {
+    tasks[i].index = i;
   }
+  localStorage.setItem('tasks', JSON.stringify(tasks));
+};
 
-  //Store modified array in localstorage
-  // localStorage.setItem('tasks', JSON.stringify(tasks));
-}
-
-window.document.addEventListener('DOMContentLoaded', (e) => {
-
-  // e.preventDefault();
-
-  
+window.document.addEventListener('DOMContentLoaded', () => {
+  let indexV = 0;
 
   const tasks = getTasks();
-  tasks.forEach((task) => addTask(task));
-
-  updateIndex();
+  tasks.forEach((task) => {
+    addTask(task);
+    indexV += 1;
+  });
 
   const addItem = document.querySelector('.add-item');
   const inputItem = document.querySelector('#input-item');
 
-  addItem.addEventListener('click', (e) => {
-    console.log(e)
-    const tasksObject = new Tasks(inputItem.value, false);
-    // console.log(inputItem.value === '');
+  addItem.addEventListener('click', () => {
     if (inputItem.value !== '') {
+      const tasksObject = new Tasks(indexV, inputItem.value, false);
       addTask(tasksObject);
+      indexV += 1;
+      updateIndex();
       addTaskLocalStorage(tasksObject);
       clearTaskInputs();
-      // addTaskLocalStorage(inputItem.value);
     } else {
       //
     }
@@ -168,10 +111,18 @@ window.document.addEventListener('DOMContentLoaded', (e) => {
 
   taskDots.forEach((element, index) => {
     element.addEventListener('click', (e) => {
-      // console.log(e.target.parentNode.parentNode)
       const node = e.target.parentNode.parentNode;
       deleteTask(node);
+      indexV -= 1;
       deleteTaskLocalStorage(index);
+      updateIndex();
+    });
+
+    const parent = element.parentNode.parentNode;
+    const textAreaContent = parent.querySelector('textarea');
+
+    textAreaContent.addEventListener('blur', () => {
+      editTask(index, textAreaContent.value);
     });
   });
 });
