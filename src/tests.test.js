522fb46a7acd4dce9f33e4af126bde79529f9f74
manq2010 <manq2010@gmail.com>
2022-11-09 14:59:42 +0200

    Add test.js file and add babel dependencies

---
+++ src/tests.test.js
@@ -0,0 +1,16 @@
+
+describe('add to local storage', () => {
+
+});
+
+describe('add to userinterface', () => {
+    
+});
+
+describe('delete from local storage', () => {
+
+});
+
+describe('delete from userinterface', () => {
+    
+});
